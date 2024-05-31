5ddb6b887785468499920ff32d7d7e710b3076ee
wyc9299 <daniel92616@gmail.com>
2024-05-31 22:00:16 +0800

    吳禹辰好帥

--- src/main/java/com/my/foodsafe/services/RestaurantService.java
+++ src/main/java/com/my/foodsafe/services/RestaurantService.java
@@ -1,17 +1,14 @@
 package com.my.foodsafe.services;
 
-import com.my.foodsafe.pojo.Restaurant;
 import com.my.foodsafe.repositories.IRestaurantRepository;
-import com.my.foodsafe.utilities.IImageUtility;
 import com.my.foodsafe.utilities.IUUIDGenerator;
 import com.my.foodsafe.utilities.TestUpload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 
 @Service
-public class RestaurantImpl implements IRestaurantService {
+public class RestaurantService implements IRestaurantService {
 
     @Autowired
     private IRestaurantRepository restaurantRepository;
@@ -21,8 +18,8 @@ public class RestaurantImpl implements IRestaurantService {
     private TestUpload testUpload;
 
     @Override
-    public Restaurant saveRestaurant(Restaurant restaurant, String photoReference) {
-        Restaurant tempRest = restaurantRepository.findByRestaurantName(restaurant.getRestaurantName());
+    public com.my.foodsafe.pojo.Restaurant saveRestaurant(com.my.foodsafe.pojo.Restaurant restaurant, String photoReference) {
+        com.my.foodsafe.pojo.Restaurant tempRest = restaurantRepository.findByRestaurantName(restaurant.getRestaurantName());
         if(tempRest != null){
             return tempRest;
         }
